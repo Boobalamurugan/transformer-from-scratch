cb1ce21fd450d2c9e9b6847078e85044eb85660b
Boobalamurugan <boobalamurugan.btech@gmail.com>
2026-01-17 16:37:31 +0530

    complete the encoder block

--- model.py
+++ model.py
@@ -43,3 +43,123 @@ class PositionalEncoding(nn.Module):
     def forward(self,x):
         x = x + (self.pe[:, :x.size(1), :]).requires_grad_(False)
         return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6) -> None: # eps means epsilon: a small value to avoid division by zero
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones((1,)))  # multiplicative parameter - initialized to 1
+        self.beta = nn.Parameter(torch.zeros((1,)))  # additive parameter - initialized to 0
+
+    def forward(self,x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.beta
+    
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model:int, d_ff:int, dropout:float) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model) # W2 and B2
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        ## (batch_size, seq_len, d_model) -> (batch_size, seq_len, d_ff) -> (batch_size, seq_len, d_model)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+
+class MultiHeadAttentionBlock(nn.Module):
+    
+    def __init__(self, d_model:int , h:int,dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h # h = number of attention heads
+
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model//h # dₖ = dimension of key (and query) vectors per head
+        self.w_q = nn.Linear(d_model,d_model) # Wq
+        self.w_k = nn.Linear(d_model,d_model) # Wk
+        self.w_v = nn.Linear(d_model,d_model) # Wv
+
+        self.wo = nn.Linear(d_model,d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
+    
+    @staticmethod
+    def attention(query,key,value,mask,dropout:nn.Dropout):
+        d_k = query.shape[-1]
+
+        ## (batch, h, seq_len,d_k) --> (batch, h,seq_len,seq_len)
+        attention_scores = (query @ key.transpose(-2,-1))/math.sqrt(d_k)
+        
+        if mask is not None:
+            attention_scores.masked_fill_(mask ==0,float("-inf")) 
+        attention_scores = attention_scores.softmax(dim = -1) ## (B,h,seq_len,seq_len)
+        
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ value) , attention_scores
+
+    def forward(self,q,k,v,mask):
+        query = self.w_q(q) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+        key = self.w_k(k) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+        value = self.w_v(v) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+
+        ## (Batch, Seq_len, d_model) --> (Batch,Seq_len,h,d_k) --> (Batch,h,Seq_len,d_k)
+        query = query.view(query[0],query[1],self.h,self.d_k).transpose(1,2)
+        key = key.view(key[0],key[1],self.h,self.d_k).transpose(1,2)   
+        value = value.view(value[0],value[1],self.h,self.d_k).transpose(1,2)    
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query,key,value,mask,self.dropout) 
+
+        ##  (Batch,h,Seq_len,d_k) --> (Batch,seq_len,h,d_k) --> (Batch,seq_len,d_model)
+        x = x.transpose(1,2).contiuous().view(x.shape[0],-1,self.h*self.d_k)
+
+        ## (Batch,seq_len,d_model) --> (Batch,seq_len,d_model)
+        return self.wo(x)
+
+class SkipConnection(nn.Module): ## ResidualConnection
+
+    def __int__(self,dropout:float)->None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self,x,sublayer):
+        return x+self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderBlock(nn.Module):
+
+    def __int__(self,self_attention_block:MultiHeadAttentionBlock,feedforward_block:FeedForwardBlock,dropout:float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feedforward_block = feedforward_block
+        self.skip_connections = nn.ModuleList([SkipConnection(dropout) for _ in range(2)])
+
+    def forward(self,x,src_mask):
+        x = self.skip_connections[0](x,lambda x: self.self_attention_block(x,x,x,src_mask))
+        x = self.skip_connections[1](x,self.feedforward_block)
+        return x
+
+class Encoder(nn.Module):
+
+    def __init__(self,layers:nn.ModuleList ):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self,x,mask):
+        for layer in self.layers:
+            x = layer(x,mask)
+        return self.norm(x)
+
+
+
