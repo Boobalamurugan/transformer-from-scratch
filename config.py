254879eba6e3b225d50c7a64be59ef884cd61da2
Boobalamurugan <boobalamurugan.btech@gmail.com>
2026-01-18 19:36:21 +0530

    change the config

--- config.py
+++ config.py
@@ -1,35 +1,20 @@
 from pathlib import Path
 
-# def get_config():
-#     return {
-#         'batch_size':8,
-#         'num_epochs':20,
-#         'lr':10**-4,
-#         'seq_len': 350,
-#         'd_model':512,
-#         'lang_src':'en',
-#         'lang_target':'ta',
-#         'model_dir':'weights',
-#         'model_basename':'tmodel_',
-#         'preload':None,
-#         'tokenizer_file':'tokenizer_{0}.json',
-#         'experiment_name':'runs/tmodel'
-#     }
 def get_config():
     return {
-        'batch_size': 8,          # MUST
-        'num_epochs': 20,
-        'lr': 1e-4,
-        'seq_len': 128,           # VERY IMPORTANT
-        'd_model': 256,           # VERY IMPORTANT
-        'lang_src': 'en',
-        "datasource": 'Helsinki-NLP/opus-100',
-        'lang_target': 'ta',
-        'model_dir': 'weights',
-        'model_basename': 'tmodel_',
-        'preload': 'latest',
-        'tokenizer_file': 'tokenizer_{0}.json',
-        'experiment_name': 'runs/tmodel'
+        'batch_size':8,
+        'num_epochs':20,
+        'lr':10**-4,
+        'seq_len': 350,
+        'd_model':512,
+        'lang_src':'en',
+        'datasource':'Helsinki-NLP/opus-100',
+        'lang_target':'ta',
+        'model_dir':'weights',
+        'model_basename':'tmodel_',
+        'preload':None,
+        'tokenizer_file':'tokenizer_{0}.json',
+        'experiment_name':'runs/tmodel'
     }
 
 def get_weights_file_path(config,epoch:str):
