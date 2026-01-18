2e87244f33a7798f9a7d8438d708ec3d5c4f2624
Boobalamurugan <boobalamurugan.btech@gmail.com>
2026-01-18 14:38:23 +0530

    fix the path error

--- train.py
+++ train.py
@@ -125,16 +125,30 @@ def get_all_sentences(ds,lang):
     for item in ds:
         yield item['translation'][lang]
 
-def get_or_build_tokenizer(config,ds,lang):
+def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config["tokenizer_file"].format(lang))
-    if not Path.exists(tokenizer_path):
+
+    if not tokenizer_path.exists():
+        ## CREATE DIRECTORY SAFELY
+        tokenizer_path.parent.mkdir(parents=True, exist_ok=True)
+
         tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
         tokenizer.pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens=["[UNK]","[PAD]","[SOS]","[EOS]"],min_frequency=2)
-        tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer=trainer)
+
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"],
+            min_frequency=2
+        )
+
+        tokenizer.train_from_iterator(
+            get_all_sentences(ds, lang),
+            trainer=trainer
+        )
+
         tokenizer.save(str(tokenizer_path))
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
     return tokenizer
 
 def get_ds(config):
